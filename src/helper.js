1f581cc75a979974736de7cf9527b5d700506a47
handoko <srabutdotcom@gmail.com>
2025-05-01 20:34:34 +0700

    add isHRR check to return record from stream

--- src/helper.js
+++ src/helper.js
@@ -9,7 +9,8 @@ export function parseRecord(data) {
       records.push(record);
       data.chunk = data.chunk.subarray(record.length);
       if (data.chunk.length == 0) {
-         if (records.at(-1)[0] == 23) data.done = true
+         if (records.at(-1)[0] == 23) data.done = true // if the last record is Application Data
+         if (isHRR(records.at(0))) data.done = true; // if serverHelloRecord contain helloRetryRequest
          break;
       }
    }
@@ -21,4 +22,8 @@ export function join(existing, other) {
    r.set(existing, 0);
    r.set(other, existing.length);
    return r;
+}
+
+function isHRR(record) {
+   return record.subarray(11, 43).toString() == "207,33,173,116,229,154,97,17,190,29,140,2,30,101,184,145,194,162,17,22,122,187,140,94,7,158,9,226,200,168,51,156"
 }
